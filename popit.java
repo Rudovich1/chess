8cefa593c90bcb6657ad3b42b61beb0ba727947b
Rudovich1 <Rudovichkirill@gmail.com>
2021-11-10 21:56:20 +0300

    kirill_popit

---
+++ popit.java
@@ -0,0 +1,5 @@
+public class popit {
+    public static void kkk(){
+        System.out.println("popit_suka"));
+    }
+}
--- src/Gay.java
+++
@@ -1,5 +0,0 @@
-public class Gay {
-    public static void kkk(){
-        System.out.println("yes");
-    }
-}
