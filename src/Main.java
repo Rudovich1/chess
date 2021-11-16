db867efbea31acde93e4f3d18de99d560179367e
Kirill <93838161+Rudovich1@users.noreply.github.com>
2021-11-16 04:54:32 +0300

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
-        System.out.println("Cal govna!Q");
-        System.out.println("Novoe govno");
-        System.out.println("Novoe govno1");
-        System.out.println(24);
-    }
-}
